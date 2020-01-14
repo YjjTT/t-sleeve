2fca9919607c64663d7dd800f1c55c65988c4824
YjjTT <yjjtt@tempot.com>
2020-01-14 14:44:09 +0800

    首页主题

--- model/theme.js
+++ model/theme.js
@@ -4,13 +4,44 @@
 import {Http} from "../utils/http";
 
 class Theme {
-    static async getHomeLoactionA() {
-        return await Http.request({
+    static locationA = 't-1'
+    static locationE = 't-2'
+    static locationF = 't-3'
+    static locationH = 't-4'
+
+    themes = []
+
+    async getThemes() {
+        const names = `${Theme.locationA},${Theme.locationE},${Theme.locationF},${Theme.locationH}`
+        this.themes = await Http.request({
             url: '/theme/by/names',
             data: {
-                names: 't-1'
+                names
             }
         })
+    }
+
+    async getHomeLoactionA() {
+        return this.themes.find(t => t.name === Theme.locationA)
+    }
+
+    async getHomeLoactionE() {
+        return this.themes.find(t => t.name === Theme.locationE)
+    }
+
+    // 一次性数据 加 static
+    // 需要等待代码执行,处理后续结果,才加await
+    // async 强制保证返回promise,如果是return 1,需要加async,如果本身返回promise,则不需要加async
+
+    static getHomeLocationESpu() {
+        return Theme.getThemeSpuByName(Theme.locationE)
+    }
+
+    static getThemeSpuByName(name) {
+        return Http.request({
+            url: `/theme/name/${name}/with_spu`
+        })
+        // 不包含任何实例属性
     }
 }
 
--- pages/home/home.js
+++ pages/home/home.js
@@ -2,6 +2,7 @@
 import {Theme} from "../../model/theme";
 import {Banner} from "../../model/banner";
 import {Category} from "../../model/category";
+import {Activity} from "../../model/activity";
 
 Page({
     /**
@@ -10,19 +11,39 @@ Page({
     data: {
         themeA: null,
         bannerB: null,
-        grid: []
+        gridC: [],
+        activityD: null,
+        themeE: null,
+        themeESpu: []
     },
     onLoad(options) {
         this.initAllData()
     },
     async initAllData() {
-        const themeA = await Theme.getHomeLoactionA()
+        const theme = new Theme()
+        await theme.getThemes()
+
+        const themeA = await theme.getHomeLoactionA()
+        const themeE = await theme.getHomeLoactionE()
+        let themeESpu = []
+        if(themeE.online){
+            const data = await Theme.getHomeLocationESpu()
+            if(data){
+                themeESpu = data.spu_list.splice(0,8)
+            }
+        }
+
         const bannerB = await Banner.getHomeLoactionB()
-        const grid = await Category.getGridCategory()
+        const gridC = await Category.getHomeLocationC()
+        const activityD = await Activity.getHomeLocationD()
+
         this.setData({
-            themeA: themeA[0],
+            themeA,
             bannerB,
-            grid
+            gridC,
+            activityD,
+            themeE,
+            themeESpu
         })
     },
     onPullDownRefresh: function () {
