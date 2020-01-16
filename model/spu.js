40106fbe185282f0b7aea8b1917fa10daf898199
YjjTT <yjjtt@tempot.com>
2020-01-16 17:55:16 +0800

    sku详情

---
+++ model/spu.js
@@ -0,0 +1,12 @@
+import {Http} from "../utils/http";
+
+class Spu{
+    static getDetail(id) {
+        return Http.request({
+            url: `/spu/id/${id}/detail`
+        })
+    }
+}
+export {
+    Spu
+}
